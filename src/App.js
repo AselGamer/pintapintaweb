424986fbede701f1da31373fc74ad581a8b523cb
javi <javi@almi.eus>
2024-11-08 15:08:58 +0100

    Javi actualizacion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import ListadoFaltas from './components/ListadoFaltas'; // Importar ListadoFaltas
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        {/* Ruta principal del login */}
+        <Route path="/" element={<Login />} />
+        
+        {/* Ruta del Dashboard con los cursos */}
+        <Route path="/dashboard/:course" element={<Dashboard />} />
+        
+        {/* Ruta para mostrar las faltas de un curso y asignatura */}
+        <Route path="/faltas/:course/:asignatura" element={<ListadoFaltas />} />
+      </Routes>
+    </Router>
   );
 }
 
